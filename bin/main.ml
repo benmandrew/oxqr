a80c874b2539a31c116edf7663b3a4a43055fbfc
benmandrew <benmandrew@gmail.com>
2025-12-22 16:26:51 +0000

    Add error-correction info table

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,9 @@
 open Oxqr
 
 let generate s ecl =
-  let config, capacity = Config.get_config_and_capacity s ecl in
-  let buf = Buf.create ~bytes:(capacity + 10) in
-  (* Add mode indicator for Alphanumeric mode (4 bits). *)
-  Buf.add_bits buf Config.mode_indicator Config.mode_indicator_length;
-  (* Add character count indicator. *)
-  let cci_len = Config.char_count_indicator_length config in
-  Buf.add_bits buf (String.length s) cci_len;
-  "Hello, World!" (* Placeholder implementation *)
+  let config, _capacity = Config.get_config_and_capacity s ecl in
+  (* TODO: Implement QR code encoding *)
+  Printf.sprintf "Config: version=%d" config.version
 
 let () =
   let qr_code = generate "HELLO WORLD" Config.ECL.M in
--- lib/config.mli
+++ lib/config.mli
@@ -10,6 +10,16 @@ type t = { version : int; ecl : ECL.t }
 val make : version:int -> ecl:ECL.t -> t
 val compare : t -> t -> int
 val char_count_indicator_length : t -> int
+
+type ec_info = {
+  ec_codewords_per_block : int;
+  group1_blocks : int;
+  group1_data_codewords : int;
+  group2_blocks : int;
+  group2_data_codewords : int;
+}
+
 val mode_indicator_length : int
 val mode_indicator : int
 val get_config_and_capacity : string -> ECL.t -> t * int
+val get_ec_info : t -> ec_info
