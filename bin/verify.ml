d7213621dd6180e82af8b3ee95eb13b1ed013a58
benmandrew <benmandrew@gmail.com>
2026-01-03 16:01:49 +0000

    feat: Verify zero-alloc with `Runtime_events`

---
+++ bin/verify.ml
@@ -0,0 +1,55 @@
+open Oxqr
+open Base
+
+let random_alphanumeric_string len =
+  let chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:" |> String.to_list
+  in
+  let chars_len = List.length chars in
+  let rec build_string acc n =
+    if n <= 0 then acc
+    else
+      let idx = Random.int chars_len in
+      let c = List.nth_exn chars idx in
+      build_string (acc ^ String.of_char c) (n - 1)
+  in
+  build_string "" len
+
+let alloc_count = ref 0
+
+let runtime_begin _device _ts phase =
+  Stdlib.Printf.printf "Begin event: %s\n%!"
+    (Runtime_events.runtime_phase_name phase)
+
+let runtime_end _device _ts phase =
+  Stdlib.Printf.printf "End event: %s\n%!"
+    (Runtime_events.runtime_phase_name phase)
+
+let runtime_counter _device _ts counter n =
+  Stdlib.Printf.printf "Counter event: %s with n=%d\n%!"
+    (Runtime_events.runtime_counter_name counter)
+    n;
+  match counter with
+  | Runtime_events.EV_C_MINOR_ALLOCATED -> Int.incr alloc_count
+  | _ -> ()
+
+let () =
+  let str_len = 100 in
+  let test_data = random_alphanumeric_string str_len in
+  let arena = Encoding.Arena.create (Some str_len) in
+  Runtime_events.start ();
+  let cursor = Runtime_events.create_cursor None in
+  let callbacks =
+    Runtime_events.Callbacks.create ~runtime_begin ~runtime_end ~runtime_counter
+      ()
+  in
+  let i = ref 0 in
+  while !i < 10000 do
+    Encoding.generate_qr_stack arena test_data Config.ECL.M;
+    Int.incr i
+  done;
+  let n_events = Runtime_events.read_poll cursor callbacks None in
+  Runtime_events.free_cursor cursor;
+  Stdlib.Printf.printf "Number of runtime events collected: %d\n" n_events;
+  Stdlib.Printf.printf "Total allocations during QR generation: %d\n"
+    !alloc_count
